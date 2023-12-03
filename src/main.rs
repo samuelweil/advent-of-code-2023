0e29f9532635d438f0fa224fd486c60b5a6a945f
Sam Weil <s.weil9391@gmail.com>
2023-12-02 21:36:17 -0600

    Add command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+use std::{env, process::exit};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Please provide a day number");
+        exit(1);
+    }
+
+    let day = args[1].parse::<u32>().expect("Please provide a valid day number");
+    println!("Running day {}", day);
 }
 
 #[cfg(test)]
