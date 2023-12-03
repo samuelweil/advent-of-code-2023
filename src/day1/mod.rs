30e76aa64b112f976be5d4e227add80782d29f90
Sam Weil <s.weil9391@gmail.com>
2023-12-03 01:57:56 -0600

    Refactor to read lines one at a time
    To avoid reading the entire file twice the CalibrationParser needs to be
    able to take each line individually. That way we can compute both days
    at once and only store the lines long enough to extract the integer
    value.

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,15 +1,35 @@
 use regex::{Match, Regex};
 
-pub fn run<'a, T: Iterator<Item = String>>(inputs: T) {
-    let result = calibration(inputs).iter().sum::<i32>();
-    println!("Day 1, Star 1: {}", result);
+const CHAR_REGEX: &str = r"\d";
+const ALL_REGEX: &str = r"\d|one|two|three|four|five|six|seven|eight|nine";
+
+pub fn run<'a, T>(inputs: T)
+where
+    T: Iterator<Item = String>,
+{
+    let mut parser_1 = CalibrationParser::new(CHAR_REGEX);
+    let mut parser_2 = CalibrationParser::new(ALL_REGEX);
+
+    let mut calib_1 = 0;
+    let mut calib_2 = 0;
+
+    for line in inputs {
+        if let Some(i1) = parser_1.parse_line(&line) {
+            calib_1 += i1;
+        }
+        if let Some(i2) = parser_2.parse_line(&line) {
+            calib_2 += i2;
+        }
+    }
+
+    println!("Day 1, Star 1: {}", calib_1);
+    println!("Day 1, Star 2: {}", calib_2);
 }
 
-struct DigitFinder(Regex);
+struct CalibrationParser(Regex);
 
 fn parse_digit(m: Match<'_>) -> Option<u32> {
     match m.as_str() {
-        "zero" => return Option::Some(0),
         "one" => return Option::Some(1),
         "two" => return Option::Some(2),
         "three" => return Option::Some(3),
@@ -19,17 +39,17 @@ fn parse_digit(m: Match<'_>) -> Option<u32> {
         "seven" => return Option::Some(7),
         "eight" => return Option::Some(8),
         "nine" => return Option::Some(9),
-        c => c.parse::<u32>().map_or(Option::None, Option::Some)
+        c => c.parse::<u32>().map_or(Option::None, Option::Some),
     }
 }
 
-impl DigitFinder {
-    fn new() -> DigitFinder {
-        let re = Regex::new(r"\d|zero|one|two|three|four|five|six|seven|eight|nine").unwrap();
-        DigitFinder(re)
+impl CalibrationParser {
+    fn new(pattern: &str) -> CalibrationParser {
+        let re = Regex::new(pattern).unwrap();
+        CalibrationParser(re)
     }
 
-    fn digits(&self, line: &str) -> Option<(u32, u32)> {
+    pub fn parse_line(&mut self, line: &str) -> Option<i32> {
         let matches: Vec<Match> = self.0.find_iter(line).collect();
 
         if matches.len() < 1 {
@@ -39,29 +59,18 @@ impl DigitFinder {
         let first_digit = parse_digit(matches[0])?;
         let last_digit = parse_digit(matches[matches.len() - 1])?;
 
-        Some((first_digit, last_digit))
-    }
-}
-
-fn calibration<'a, T: Iterator<Item = String>>(doc: T) -> Vec<i32> {
-    let mut result = Vec::new();
-    let finder = DigitFinder::new();
-
-    for line in doc {
-        let (first_digit, last_digit) = finder.digits(&line).unwrap();
-        let configuration = format!("{}{}", first_digit, last_digit)
+        let config_value = format!("{}{}", first_digit, last_digit)
             .parse::<i32>()
-            .unwrap();
-        result.push(configuration);
-    }
+            .map_or(None, Some)?;
 
-    result
+        Some(config_value)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
+    use crate::day1::CalibrationParser;
 
     const INPUT_1: &str = "1abc2
     pqr3stu8vwx
@@ -70,7 +79,13 @@ mod tests {
 
     #[test]
     fn test_parsing_number() {
-        let result = calibration(INPUT_1.lines().map(String::from));
+        let mut parser = CalibrationParser::new(CHAR_REGEX);
+        let result = INPUT_1
+            .lines()
+            .map(|s| parser.parse_line(s))
+            .filter_map(|s| s)
+            .collect::<Vec<i32>>();
+
         assert_eq!(vec![12, 38, 15, 77], result);
     }
 
@@ -84,7 +99,22 @@ mod tests {
 
     #[test]
     fn test_parsing_digits_by_name() {
-        let result = calibration(INPUT_2.lines().map(String::from));
+        let mut parser = CalibrationParser::new(ALL_REGEX);
+        let result = INPUT_2
+            .lines()
+            .map(|s| parser.parse_line(s))
+            .filter_map(|s| s)
+            .collect::<Vec<i32>>();
+
         assert_eq!(vec![29, 83, 13, 24, 42, 14, 76], result);
+        assert_eq!(281, result.iter().sum());
+
+        let result_2 = INPUT_1
+            .lines()
+            .map(|s| parser.parse_line(s))
+            .filter_map(|s| s)
+            .collect::<Vec<i32>>();
+
+        assert_eq!(vec![12, 38, 15, 77], result_2);
     }
 }
