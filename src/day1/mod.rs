58142c44c06bcf1ee731b23e5ae3f282830bb3c1
Sam Weil <s.weil9391@gmail.com>
2023-12-02 23:10:12 -0600

    Use regex to parse digits

--- src/day1/mod.rs
+++ src/day1/mod.rs
@@ -1,27 +1,42 @@
+use regex::{Match, Regex};
+
 pub fn run<'a, T: Iterator<Item = String>>(inputs: T) {
     let result = calibration(inputs).iter().sum::<i32>();
     println!("Day 1, Star 1: {}", result);
 }
 
-fn is_digit(c: char) -> bool {
-    c.is_digit(10)
+struct DigitFinder(Regex);
+
+fn parse_digit(m: Match<'_>) -> Option<u32> {
+    m.as_str().parse::<u32>().map_or(Option::None, Option::Some)
 }
 
-fn read_digit(line: &str, at_index: usize) -> u32 {
-    line.chars()
-        .nth(at_index)
-        .map(|c| c.to_digit(10).unwrap())
-        .unwrap()
+impl DigitFinder {
+    fn new() -> DigitFinder {
+        let re = Regex::new(r"\d").unwrap();
+        DigitFinder(re)
+    }
+
+    fn digits(&self, line: &str) -> Option<(u32, u32)> {
+        let matches: Vec<Match> = self.0.find_iter(line).collect();
+
+        if matches.len() < 1 {
+            return Option::None;
+        }
+
+        let first_digit = parse_digit(matches[0])?;
+        let last_digit = parse_digit(matches[matches.len() - 1])?;
+
+        Some((first_digit, last_digit))
+    }
 }
 
 fn calibration<'a, T: Iterator<Item = String>>(doc: T) -> Vec<i32> {
     let mut result = Vec::new();
+    let finder = DigitFinder::new();
 
     for line in doc {
-        let get_int = |index: usize| read_digit(line.as_str(), index);
-
-        let first_digit = line.find(is_digit).map(get_int).unwrap();
-        let last_digit = line.rfind(is_digit).map(get_int).unwrap();
+        let (first_digit, last_digit) = finder.digits(&line).unwrap();
         let configuration = format!("{}{}", first_digit, last_digit)
             .parse::<i32>()
             .unwrap();
@@ -33,15 +48,17 @@ fn calibration<'a, T: Iterator<Item = String>>(doc: T) -> Vec<i32> {
 
 #[cfg(test)]
 mod tests {
-    const INPUT: &str = "1abc2
+
+    use super::*;
+
+    const INPUT_1: &str = "1abc2
     pqr3stu8vwx
     a1b2c3d4e5f
     treb7uchet";
 
     #[test]
-    fn test_main() {
-        let result = super::calibration(INPUT.lines().map(String::from));
-
+    fn test_parsing_number() {
+        let result = calibration(INPUT_1.lines().map(String::from));
         assert_eq!(vec![12, 38, 15, 77], result);
     }
 }
