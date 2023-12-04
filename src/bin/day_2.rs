89a5d2f6c3ee07856ba921520a8225e43a880ecf
Sam Weil <s.weil9391@gmail.com>
2023-12-04 01:33:11 -0600

    Add solution for day 2
    - Sum of ids for games possible w/ specified set
    - Sum powers of minimum hands required for each game

--- src/bin/day_2.rs
+++ src/bin/day_2.rs
@@ -1,3 +1,4 @@
+use advent_of_code_2023::io::read_lines;
 use once_cell::sync::Lazy;
 use regex::Regex;
 
@@ -7,7 +8,24 @@ fn main() {
         blue: 14,
         green: 13,
     };
+
+    let mut sum_valid_game_ids = 0;
+    let mut sum_powers = 0;
+
+    for line in read_lines("inputs/day_2.txt") {
+        let game = Game::from(&line);
+        if game.is_valid(&max_val_hand) {
+            sum_valid_game_ids += game.id;
+        }
+
+        sum_powers += game.min_val_hand().power();
+    }
+
+    println!("Day 2, Star 1: {}", sum_valid_game_ids);
+    println!("Day 2, Star 2: {}", sum_powers);
 }
+
+
 
 #[derive(PartialEq, Debug)]
 struct Hand {
@@ -46,6 +64,10 @@ impl Hand {
         }
         set
     }
+
+    fn power(&self) -> i32 {
+        self.red * self.blue * self.green
+    }
 }
 
 struct Game {
@@ -78,12 +100,61 @@ impl Game {
 
         Game { id, hands }
     }
+
+    fn is_valid(&self, max_hand: &Hand) -> bool {
+        for hand in &self.hands {
+            if hand.red > max_hand.red || hand.blue > max_hand.blue || hand.green > max_hand.green {
+                return false;
+            }
+        }
+        true
+    }
+
+    fn min_val_hand(&self) -> Hand {
+        let mut min_hand = Hand {
+            red: 0,
+            blue: 0,
+            green: 0
+        };
+
+        for hand in &self.hands {
+            if hand.red > min_hand.red {
+                min_hand.red = hand.red;
+            }
+            if hand.blue > min_hand.blue {
+                min_hand.blue = hand.blue;
+            }
+            if hand.green > min_hand.green {
+                min_hand.green = hand.green;
+            }
+        }
+
+        min_hand
+    }
+
 }
 
 #[cfg(test)]
 mod test {
-
     use super::*;
+
+    fn valid_games<T: Iterator<Item=U>, U: AsRef<str>>(lines: T, max_val_hand: &Hand) -> Vec<i32> {
+        lines.filter_map(|line| {
+            let game = Game::from(line.as_ref());
+            if game.is_valid(max_val_hand) {
+                Some(game.id)
+            } else {
+                None
+            }
+        }).collect()
+    }
+    
+    fn powers<T: Iterator<Item=U>, U: AsRef<str>>(lines: T) -> Vec<i32> {
+        lines.map(|line| {
+            let game = Game::from(line.as_ref());
+            game.min_val_hand().power()
+        }).collect()
+    }
 
     #[test]
     fn test_parsing_game_id() {
@@ -128,4 +199,43 @@ mod test {
             }
         );
     }
+
+    const TEST_INPUT: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
+
+    #[test]
+    fn test_valid_games() {
+        let max_val_hand = Hand {
+            red: 12,
+            blue: 14,
+            green: 13,
+        };
+
+        let result = valid_games(TEST_INPUT.lines(), &max_val_hand);
+        assert_eq!(vec![1, 2, 5], result);
+    }
+
+    #[test]
+    fn test_lowest_hand() {
+        let game = Game::from("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
+        let result = game.min_val_hand();
+        assert_eq!(result, Hand { red: 4, blue: 6, green: 2 });
+    }
+
+    #[test]
+    fn test_power_of_hand() {
+        let hand = Hand { red: 4, blue: 6, green: 2 };
+        let result = hand.power();
+        assert_eq!(result, 48);
+    }
+
+    #[test]
+    fn test_powers() {
+        let result = powers(TEST_INPUT.lines());
+        assert_eq!(vec![48, 12, 1560, 630, 36], result);
+    }
+
 }
