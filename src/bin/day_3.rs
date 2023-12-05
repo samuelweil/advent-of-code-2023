410bc618f4a6e90d7267cd8d5b9988c432797f8c
Sam Weil <s.weil9391@gmail.com>
2023-12-04 21:25:20 -0600

    Add filtering for valid part numbers

--- src/bin/day_3.rs
+++ src/bin/day_3.rs
@@ -1,24 +1,48 @@
+use std::cmp::max;
+
 use advent_of_code_2023::io::read_lines;
 use once_cell::sync::Lazy;
 use regex::Regex;
 
 fn main() {
-    let mut parts = Vec::new();
-    let mut symbols = Vec::new();
+    let (parts, symbols) = parse_schematic(read_lines("inputs/day_3.txt"));
+    let valid_parts = valid_parts(parts, &symbols);
+    let result = valid_parts.iter().map(|p| p.number).sum::<i32>();
+    println!("Day 3, Star 1: {}", result);
+}
 
-    for (line_no, line) in read_lines("data/day_3.txt").enumerate() {
-        let (p, s) = parse_schematic(line_no, &line);
-        parts.extend(p);
-        symbols.extend(s);
-    }
+fn valid_parts(parts: Vec<Part>, symbols: &[Symbol]) -> Vec<Part> {
+    parts
+        .into_iter()
+        .filter(|p| is_adjacent(p, symbols))
+        .collect::<Vec<_>>()
+}
+
+fn is_adjacent(part: &Part, symbols: &[Symbol]) -> bool {
+    symbols.into_iter().any(|s| part.is_adjacent(s))
 }
 
 #[derive(PartialEq, Debug)]
-struct PartNumber {
-    value: i32,
+struct Part {
+    number: i32,
     row: usize,
     start: usize,
     end: usize,
+}
+
+impl Part {
+    fn is_adjacent(&self, symbol: &Symbol) -> bool {
+        let start_col = max(self.start, 1) - 1;
+        let end_col = self.end; // Match goes 1 past the end
+
+        let start_row = max(self.row, 1) - 1;
+        let end_row = self.row + 1;
+
+        let vertical_adjacent = symbol.row <= end_row && symbol.row >= start_row;
+        let horizontal_adjacent = symbol.column <= end_col && symbol.column >= start_col;
+
+        vertical_adjacent && horizontal_adjacent
+    }
 }
 
 #[derive(PartialEq, Debug)]
@@ -30,7 +54,22 @@ struct Symbol {
 static PART_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"\d+").unwrap());
 static SYMBOL_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"[^\d\.]").unwrap());
 
-fn parse_schematic(line_no: usize, line: &str) -> (Vec<PartNumber>, Vec<Symbol>) {
+fn parse_schematic<I: AsRef<str>, T: Iterator<Item = I>>(
+    schematic: T,
+) -> (Vec<Part>, Vec<Symbol>) {
+    let mut parts = Vec::new();
+    let mut symbols = Vec::new();
+
+    for (line_no, line) in schematic.enumerate() {
+        let (p, s) = parse_schematic_line(line_no, line.as_ref().trim());
+        parts.extend(p);
+        symbols.extend(s);
+    }
+
+    (parts, symbols)
+}
+
+fn parse_schematic_line(line_no: usize, line: &str) -> (Vec<Part>, Vec<Symbol>) {
     let parts = PART_REGEX
         .find_iter(line)
         .map(|m| {
@@ -38,8 +77,8 @@ fn parse_schematic(line_no: usize, line: &str) -> (Vec<PartNumber>, Vec<Symbol>)
             let start = m.start();
             let end = m.end();
 
-            PartNumber {
-                value: number,
+            Part {
+                number,
                 row: line_no,
                 start,
                 end,
@@ -63,19 +102,41 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_part_1() {
+        let input = "467..114..
+        ...*......
+        ..35..633.
+        ......#...
+        617*......
+        .....+.58.
+        ..592.....
+        ......755.
+        ...$.*....
+        .664.598..";
+
+        let (parts, symbols) = parse_schematic(input.lines());
+        let result = valid_parts(parts, &symbols)
+            .iter()
+            .map(|p| p.number)
+            .collect::<Vec<_>>();
+
+        assert_eq!(result, vec![467, 35, 633, 617, 592, 755, 664, 598]);
+    }
+
+    #[test]
     fn test_parse_parts() {
-        let (parts, symbols) = parse_schematic(1, "467..114..");
+        let (parts, symbols) = parse_schematic_line(1, "467..114..");
         assert_eq!(
             parts,
             vec![
-                PartNumber {
-                    value: 467,
+                Part {
+                    number: 467,
                     row: 1,
                     start: 0,
                     end: 3,
                 },
-                PartNumber {
-                    value: 114,
+                Part {
+                    number: 114,
                     row: 1,
                     start: 5,
                     end: 8,
@@ -87,7 +148,7 @@ mod tests {
 
     #[test]
     fn test_parse_symbols() {
-        let (parts, symbols) = parse_schematic(3, "...$.*....");
+        let (parts, symbols) = parse_schematic_line(3, "...$.*....");
 
         assert_eq!(parts.len(), 0);
         assert_eq!(
@@ -98,16 +159,46 @@ mod tests {
 
     #[test]
     fn test_parse_mixed() {
-        let (parts, symbols) = parse_schematic(8, "617*......");
+        let (parts, symbols) = parse_schematic_line(8, "617*......");
         assert_eq!(
             parts,
-            vec![PartNumber {
-                value: 617,
+            vec![Part {
+                number: 617,
                 row: 8,
                 start: 0,
                 end: 3,
             }]
         );
         assert_eq!(symbols, vec![Symbol { row: 8, column: 3 }])
+    }
+
+    #[test]
+    fn test_adjacent_top() {
+        let part = Part {
+            number: 617,
+            row: 8,
+            start: 0,
+            end: 3,
+        };
+
+        for i in 0..3 {
+            let symbol = Symbol { row: 7, column: i };
+            assert!(part.is_adjacent(&symbol), "{} should be adjacent", i);
+        }
+    }
+
+    #[test]
+    fn test_adjacent_bot() {
+        let part = Part {
+            number: 617,
+            row: 8,
+            start: 0,
+            end: 3,
+        };
+
+        for i in 0..3 {
+            let symbol = Symbol { row: 9, column: i };
+            assert!(part.is_adjacent(&symbol), "{} should be adjacent", i);
+        }
     }
 }
